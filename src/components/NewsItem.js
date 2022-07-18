47102c5d6f67479ba01b14e03c323abbf864e48d
Manishsahusahu <79519076+Manishsahusahu@users.noreply.github.com>
2022-07-18 18:40:29 +0530

    infinite scrolling

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,13 +8,13 @@ export class NewsItem extends Component {
                 <div className="card" style={{ width: "18rem" }}>
                     <img src={imgUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
                             {source.name}
-                            <span class="visually-hidden">unread messages</span>
+                            <span className="visually-hidden">unread messages</span>
                         </span>
                         <h5 className="card-title">{title ? ((title.length <= 45) ? title : title.slice(0, 45)) : title}...</h5>
                         <p className="card-text">{description ? (description.length <= 88 ? description : description.slice(0, 88)) : description}...</p>
-                        <p className="card-text"><small class="text-muted">By {author ? author : "Unknown"} {publishedAt ? `On ${new Date(publishedAt).toGMTString()}` : ""}</small></p>
+                        <p className="card-text"><small className="text-muted">By {author ? author : "Unknown"} {publishedAt ? `On ${new Date(publishedAt).toGMTString()}` : ""}</small></p>
                         <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-primary">Read More</a>
                     </div>
                 </div>
